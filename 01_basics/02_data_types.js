704084c98962d3ebb741692f406ec5cd2963cda2
The-code-wreker <125041976+The-code-wreker@users.noreply.github.com>
2024-02-12 10:01:43 +0000

    data types

---
+++ 01_basics/02_data_types.js
@@ -0,0 +1,31 @@
+"use strict"; // treat all JS code as newer version (not actually needed already applied in new engines )
+
+// alert (3+3) // we are using node js, not browser
+
+console.log (3+3); // code readability should be high
+console.log ("Hitesh")
+
+// ecma official but mdn is a good alternative
+
+let name = "manish" //string
+let age = 18
+let isLoggedIn = false
+let state;
+
+//////////primitive data types
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => khali hai
+// undefined => 
+// symbol => used in majorly react to find uniqueness
+
+
+////////// object
+
+console.log(typeof "hitesh") // string
+console.log(typeof age) // number
+console.log(typeof null)  // object no specific data type
+console.log(typeof undefined)  // undefined
+
